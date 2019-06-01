60d9fcbf294cc5da79c3beef468ac70b55ffe92a
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-01 15:50:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,8 @@ end
 def assign_rooms
   new_array = []
   counter = 1 
-  attendees.each.do
+  attendees.each.do |name|
+    
   puts "Hello #{name}! You'll be assigned to room #{room assignments}!"
   end
 end  
