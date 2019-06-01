e0179e80770ce4394b7fafc146f985775aeb7d75
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-01 15:39:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,9 @@
-def attendees
-  ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-end
-
 def batch_badge_maker(name)
   attendees.each.do |name|
   puts "Hello, my name is #{name}."
 end
 
 def assign_rooms(attendees)
+  attendees.each.do
   
   
