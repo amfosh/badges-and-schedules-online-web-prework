0f427c0c2192204b8fe4f37aa9d185d43d1b8d79
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-01 15:38:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def attendees
+  ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+end
+
+def batch_badge_maker(name)
+  attendees.each.do |name|
+  puts "Hello, my name is #{name}."
+end
+
+def assign_rooms(attendees)
+  
+  
