25047f5ee24792ffc9ff29ae65157f9355839ac9
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-01 15:48:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,8 @@ end
 
 def batch_badge_maker[attendees]
   new array = []
-  attendees.each.do 
+  attendees.each.do |name|
+    
 def assign_rooms
   attendees.each.with.index |name index|
   puts "Hello #{name}! You'll be assigned to room #{room assignments}!"
